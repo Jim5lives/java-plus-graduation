063686a7bbe6cff0b9ded747f94217ce08f5e846
Vyacheslav Tetyuev <s1995t@yandex.ru>
2025-02-20 01:02:45 +0500

    add Gateway Server

--- infra/gateway/src/main/java/ru/yandex/practicum/Gateway.java
+++ infra/gateway/src/main/java/ru/yandex/practicum/Gateway.java
@@ -1,14 +1,14 @@
-package ru.practicum.ewm;
+package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class StatApplication {
-
+@EnableDiscoveryClient
+public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(StatApplication.class, args);
+        SpringApplication.run(Gateway.class);
     }
 }
+
