4b5b56fa9eac371e2cf7ac1badf9eafc5c11b317
Vyacheslav Tetyuev <s1995t@yandex.ru>
2025-02-22 19:01:26 +0500

    fix properties to run pipeline

--- infra/gateway-server/src/main/java/ru/yandex/practicum/Gateway.java
+++ infra/gateway-server/src/main/java/ru/yandex/practicum/Gateway.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(Gateway.class);
+        SpringApplication.run(Gateway.class, args);
     }
 }
 
